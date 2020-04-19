28bbafc492c328a7e7ecb84e89eb72707c414691
Administrator <2275990862@qq.com>
2020-04-19 11:27:23 +0800

    数据结构以及并发

---
+++ src/com/offer/HasPath.java
@@ -0,0 +1,13 @@
+package com.offer;
+
+/**
+ * @program: datastructure
+ * @description: 矩阵中的路径
+ * @author: Mr.Wang
+ * @create: 2020-04-19 11:26
+ **/
+public class HasPath {
+    public boolean hasPath(char[] matrix, int rows, int cols, char[] str){
+        return true;
+    }
+}
--- src/com/offer/MovingCount.java
+++ src/com/offer/MovingCount.java
@@ -7,4 +7,28 @@ package com.offer;
  * @create: 2020-04-19 10:37
  **/
 public class MovingCount {
+    public int movingCount(int threshold, int rows, int cols){
+        int[][] flag=new int[rows][cols];
+        return helper(0,0,rows,cols,flag,threshold);
+    }
+
+    private int helper(int i, int j, int rows, int cols, int[][] flag, int threshold) {
+        if (i<0||i>=rows||j<0||j>=cols||flag[i][j]==1||numSum(i)+numSum(j)>threshold){
+            return 0;
+        }
+        flag[i][j]=1;
+        return helper(i-1,j,rows,cols,flag,threshold)+
+                helper(i+1,j,rows,cols,flag,threshold)+
+                helper(i,j-1,rows,cols,flag,threshold)+
+                helper(i,j+1,rows,cols,flag,threshold)+1;
+    }
+
+    public int numSum(int num){
+        int result=0;
+        while(num>0){
+            result+=num%10;
+            num/=10;
+        }
+        return  result;
+    }
 }
