ceb58dccc7a69b5b79c11a9ef81f5b0e41d61d6c
Administrator <2275990862@qq.com>
2020-04-19 15:06:19 +0800

    数据结构以及并发

--- src/com/offer/HasPath.java
+++ src/com/offer/HasPath.java
@@ -7,7 +7,36 @@ package com.offer;
  * @create: 2020-04-19 11:26
  **/
 public class HasPath {
-    public boolean hasPath(char[] matrix, int rows, int cols, char[] str){
+    public boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
+        boolean[] visitFlags = new boolean[matrix.length];
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (helper(matrix, rows, cols, row, col, str, 0, visitFlags)) {
+                    return true;
+                }
+            }
+        }
         return true;
+    }
+
+    private boolean helper(char[] matrix, int rows, int cols, int row, int col, char[] str, int k, boolean[] visitFlags) {
+        int index = row * cols + col;
+        if (row < 0 || row >= rows || col < 0 || col >= cols || visitFlags[index] || matrix[index] != str[k]) {
+            return false;
+        }
+        visitFlags[index] = true;
+
+        if (k == str.length - 1) {
+            return true;
+        }
+        k++;
+        if (helper(matrix, rows, cols, row-1, col, str, 0, visitFlags) ||
+                helper(matrix, rows, cols, row+1, col, str, 0, visitFlags) ||
+                helper(matrix, rows, cols, row, col-1, str, 0, visitFlags) ||
+                helper(matrix, rows, cols, row, col+1, str, 0, visitFlags)) {
+            return true;
+        }
+        visitFlags[index]=false;
+        return false;
     }
 }
